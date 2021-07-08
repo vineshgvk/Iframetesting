4658f51d8e0ec241b3c1a69b94b83e6d8d9cdd89
vineshgvk <67590588+vineshgvk@users.noreply.github.com>
2021-07-08 19:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var password=document.getElementById("password").value;
                             "email": username,
                             "gender": "male",
                             "customAttributes" : {
-                                                "DOB": 909426600000,
+                                                
                                                 "Useractive" :"yes"
                                               }
 
