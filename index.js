0b24208d9acd0049c5e186f96b8a495fb8883ac4
vineshgvk <67590588+vineshgvk@users.noreply.github.com>
2021-08-11 19:56:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-K0EYK7ZWBNRU-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-E26WCXZROOUQ-2-3");
 
 function validate(username)
 {
