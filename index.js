065e656bc0f2d6ffc8344ddfd597315c95df91f4
vineshgvk <67590588+vineshgvk@users.noreply.github.com>
2021-09-17 23:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-K0EYK7ZWBNRU-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-K0EYK7ZWBNRU-2-2");
 
 function validate(username)
 {
@@ -13,7 +13,7 @@ var password=document.getElementById("password").value;
         a1: "PX_Company-1",
         a2: "PX_Company-2",
         a3: "PX_Company-3",
-        a4: "PX_Company-4"
+        a4: "PX_Company-stage"
 
     }
     
